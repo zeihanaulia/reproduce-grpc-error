d30b494a75d52e48ca9e4e5b73d36e5ff996899e
Zeihan Aulia <zeihan@warungpintar.co>
2021-08-18 10:57:21 +0700

    initial commit

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/zeihanaulia/reproduce-grpc-error
+
+go 1.16
+
+require (
+	github.com/go-chi/chi/v5 v5.0.3
+	github.com/golang/protobuf v1.4.3
+	google.golang.org/grpc v1.40.0
+	google.golang.org/protobuf v1.25.0
+)
---
+++ http-server/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	pb "github.com/zeihanaulia/reproduce-grpc-error/proto/helloworld"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/backoff"
+)
+
+const (
+	address     = "localhost:50051"
+	defaultName = "world"
+)
+
+func main() {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	r.Get("/panic", func(w http.ResponseWriter, r *http.Request) {
+		panic("error")
+	})
+
+	r.Get("/connect", func(w http.ResponseWriter, r *http.Request) {
+		// Set up a connection to the server.
+		conn, err := grpc.Dial(address,
+			grpc.WithInsecure(), grpc.WithConnectParams(grpc.ConnectParams{
+				Backoff:           backoff.DefaultConfig,
+				MinConnectTimeout: 5 * time.Second,
+			}))
+		if err != nil {
+			log.Println(fmt.Errorf("did not connect: %v", err))
+		}
+		// defer conn.Close()
+		c := pb.NewGreeterClient(conn)
+
+		// ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+		// defer cancel()
+		ctx := context.Background()
+		name := "test"
+		resp, err := c.SayHello(ctx, &pb.HelloRequest{Name: name})
+		if err != nil {
+			// log.Fatalf("could not greet: %v", err)
+			log.Println(fmt.Errorf("could not greet: %v", err))
+		}
+		log.Printf("Greeting: %s", resp.GetMessage())
+
+	})
+
+	log.Printf("http server listening at %s", ":3000")
+	if err := http.ListenAndServe(":3001", r); err != nil {
+		panic(fmt.Errorf("cannot start server, err: %v", err))
+	}
+}
